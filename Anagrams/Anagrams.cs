49c8c9777484c281c68f47cdc246fd830e498899
Nicholas Underwood <underwnm@gmail.com>
2016-10-18 17:55:12 -0500

    Start of Anagrams
    Started logic for anagrams.

---
+++ Anagrams/Anagrams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagrams
+{
+    class FindAnagrams
+    {
+        string[] textFile;
+        
+        public void ExecuteAnagramCheck()
+        {
+            ReadFromTextFile();
+            
+
+            foreach (string textFile in textFile)
+            {
+                Console.WriteLine("\t" + textFile);
+            }
+        }
+        public void ReadFromTextFile()
+        {
+            textFile = File.ReadAllLines(@"C:\scrabbleDictionary.txt");
+        }
+        public string AlphabetizeString(string textFile)
+        {
+            char[] a = textFile.ToCharArray();
+            Array.Sort(a);
+            return new string(a);
+        }
+    }
+}
---
+++ Anagrams/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindAnagrams findAnagrams = new FindAnagrams();
+            findAnagrams.ExecuteAnagramCheck();
+
+            Console.WriteLine("END OF PROGRAM");
+            Console.ReadKey();
+        }
+    }
+}
